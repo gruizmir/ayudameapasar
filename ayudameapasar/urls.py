3a92c13b00c2b9763c0c7de8aaeca694ed7c902c
Gabriel Ruiz M <gabriel.ruiz.miranda@gmail.com>
2013-10-16 11:53:28 -0300

    Agregado proyecto base de ayudameapasar.cl. Incluye configuraciones de admin y archivos estáticos.

---
+++ ayudameapasar/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #url(r'^$', 'main.views.mainView'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
